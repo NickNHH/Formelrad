43bf27f72ab4c79566717fbfd8933811fd17814b
LorisSchranz <schranz.loris@gmail.com>
2018-11-05 08:05:05 +0100

    Test123

--- src/application/Calculator.java
+++ src/application/Calculator.java
@@ -6,50 +6,59 @@ package application;
  * @version 13.09.2018
  */
 public class Calculator {
-	private double power;
-	private double tension;
-	private double current;
-	private double resistance;
-	
-	Calculator(double power, double tension, double current, double resistance) {
-		super();
-		this.power = power;
-		this.tension = tension;
-		this.current = current;
-		this.resistance = resistance;
-	}
-	
-	double getPower() {
-		return power;
-	}
-	
-	double getTension() {
-		return tension;
-	}
+    private double power;
+    private double tension;
+    private double current;
+    private double resistance;
 
-	double getCurrent() {
-		return current;
-	}
+    Calculator(double power, double tension, double current, double resistance) {
+        super();
+        this.power = power;
+        this.tension = tension;
+        this.current = current;
+        this.resistance = resistance;
+    }
 
-	double getResistance() {
-		return resistance;
-	}
+    double getPower() {
+        return power;
+    }
 
-	@Override
-	public String toString() {
-		return "Calculator [power=" + power +
-				", tension=" + tension +
-				", current=" + current +
-				", resistance="	+ resistance + "]";
-	}
+    double getTension() {
+        return tension;
+    }
 
-	void calculate() {
-		/* Hier auf Grund der vorhanden Werte entscheiden
-		 * welche Methode unten aufgerufen werden muss.
-		 */
+    double getCurrent() {
+        return current;
+    }
+
+    double getResistance() {
+        return resistance;
+    }
+
+    @Override
+    public String toString() {
+        return "Calculator [power=" + power +
+                ", tension=" + tension +
+                ", current=" + current +
+                ", resistance=" + resistance + "]";
+    }
+
+    void calculate() {
+        calculateCurrent(tension, resistance, power);
+    }
+
+    private void calculateCurrent(double tension, double resistance, double power){
+    	//Calculate
+		if(tension == 0.00){
+			current = sqrt(power / resistance);
+		}
+
+		if(resistance == 0.00){
+			current = power / tension;
+		}
+
+		if(power == 0.00) {
+            current = tension / resistance;
+        }
 	}
-	
-	/* Hier die Methoden mit den Formlen hinzufügen
-	 */
-	
 }
