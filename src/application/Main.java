0b494c41be9ef4e34adf199aa72e7da1b65622dc
LorisSchranz <schranz.loris@gmail.com>
2018-11-19 08:36:38 +0100

    Implementing everything for the red text

--- src/application/Main.java
+++ src/application/Main.java
@@ -42,7 +42,7 @@ public class Main extends Application {
 			TextField tfPower = new TextField();
 			tfPower.relocate(100, 285);
 			tfPower.setFont(Font.font("Verdana", 15));
-			tfPower.setStyle("-fx-text-fill: red;");
+			tfPower.setStyle("-fx-text-inner-color: red;");
 			root.getChildren().add(tfPower);
 
 			Label lbTension = new Label("Spannung:");
@@ -53,7 +53,7 @@ public class Main extends Application {
 			TextField tfTension = new TextField();
 			tfTension.relocate(100, 325);
 			tfTension.setFont(Font.font("Verdana", 15));
-			tfPower.setStyle("-fx-text-fill: red;");
+			tfPower.setStyle("-fx-text-inner-color: red;");
 			root.getChildren().add(tfTension);
 
 			Label lbCurrent = new Label("Strom:");
@@ -64,7 +64,7 @@ public class Main extends Application {
 			TextField tfCurrent = new TextField();
 			tfCurrent.relocate(100, 365);
 			tfCurrent.setFont(Font.font("Verdana", 15));
-			tfPower.setStyle("-fx-text-fill: red;");
+			tfPower.setStyle("-fx-text-inner-color: red;");
 			root.getChildren().add(tfCurrent);
 
 			Label lbResistance = new Label("Widerstand:");
@@ -75,7 +75,7 @@ public class Main extends Application {
 			TextField tfResistance = new TextField();
 			tfResistance.relocate(100, 405);
 			tfResistance.setFont(Font.font("Verdana", 15));
-			tfPower.setStyle("-fx-text-fill: red;");
+			tfPower.setStyle("-fx-text-inner-color: red;");
 			root.getChildren().add(tfResistance);
 
 			Button btCalculate = new Button();
